32709da82fc9aae604cd81893011d25ea399bb2a
lucas.fontaine@student-cs.fr <lucas.fontaine@student-cs.fr>
2023-09-13 17:37:58 +0200

    Adding Shift Register

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+#include "shift.cpp"
+
+
 int main(){
-    std::cout << "Hello World !" << std::endl;
+    Shift s = Shift(0);
+
+    s.fill(0xcafefbfb);
+    
+    int b = 0;
+    for(int i = 0; i < 8; i++){
+        b = (b << 1) + s.shift(0);
+    }
+
+    std::cout << "Buffer : " << std::hex << s.get() << std::endl;
+    std::cout << "b : " << b << std::endl;
+
+    //std::cout << "shift buffer : " << std::hex << s.get() << std::endl;
 
     return 0;
 }
---
+++ src/shift.cpp
@@ -0,0 +1,55 @@
+#ifndef _SHIFTER
+#define _SHIFTER
+
+#include <cstdint>
+
+class Shift{
+    // 32 bit shift register
+
+    private:
+        uint32_t buffer;
+        bool direction;
+    
+
+    public:
+        Shift(bool direction){
+            // The only option is the shift direction
+            // We define :
+            //    0 -> left shift
+            //    1 -> right shift
+
+            this->direction = direction;
+        }
+
+        void fill(uint32_t input){
+            // Fill buffer with the input
+            this->buffer = input;
+        }
+
+        uint32_t get(){
+            return this->buffer;
+        }
+
+        bool shift(bool input){
+            // Shift the register according to self.direction
+            // Input is the new bit being shifted in
+            // Output is the overflow bit
+
+            bool res;
+
+            if(this->direction){
+                // Right shift
+                res = this->buffer & 0b1;
+                this->buffer = (this->buffer >> 1) + (input << 31);
+            }
+            else {
+                // Left shift
+                res = this->buffer & (const uint32_t)(1<<31);
+                this->buffer = (this->buffer << 1) + input;
+            }
+
+            return res;
+        }
+};
+
+#endif
