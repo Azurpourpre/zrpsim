0836d601eca470c72310ba74dc597b2c68fa7451
lucas.fontaine@student-cs.fr <lucas.fontaine@student-cs.fr>
2023-09-20 14:12:12 +0200

    Implemented MOV (IRQ is defined but not implemented)

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -405,7 +405,37 @@ class StateMachine {
                 }
             }
 
-    
+            void irq(const uint16_t IR){
+                TODO("IRQ");
+            }
+
+            void set(const uint16_t IR){
+                //Set the stall
+                this->stall_counter = (IR & delay_sideset_mask) >> 8;
+
+                const uint8_t data = IR & 0b11111;
+
+                switch( (IR & 0b11100000) >> 5){
+                    case 0b000:
+                        TODO("GPIO");
+                        break;
+                    case 0b001:
+                        this->X = data;
+                        break;
+                    case 0b010:
+                        this->Y = data;
+                        break;
+                    case 0b100:
+                        TODO("GPIO");
+                        break;
+                    case 0b011:
+                    case 0b101:
+                    case 0b110:
+                    case 0b111:
+                        std::cerr << "Reserved destination in SET" << std::endl;
+                        break;
+                }
+            }
 
 };
 
